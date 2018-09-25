4227727f0475c282c175ceffc01e0929ea967fd4
martin-king <martin-king@users.noreply.github.com>
2018-09-25 21:07:51 +0200

    Delete sleeprun.s

--- monashubuntudir_velvor2dperiodichorconvtankdir/sleeprun.s
+++
@@ -1,8 +0,0 @@
-./velvor2dhorconvtank_sillyopenmp.exe < ./inputr1.s
-sleep 10
-./velvor2dhorconvtank_sillyopenmp.exe < ./inputr2.s 
-sleep 10
-./velvor2dhorconvtank_sillyopenmp.exe < ./inputr3.s
-sleep 10
-./velvor2dhorconvtank_sillyopenmp.exe < ./inputr4.s
-
